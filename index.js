14cac0783bac561716c690b11670de9c5c87b502
valeviit <valeviit@icloud.com>
2022-11-18 12:02:37 +0100

    point 7 - add a new /animals route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res)=>{
     res.send("<h1>Adopt a Pet!</h1><p>Browse through the links below to find your new furry friend:</p><ul><li>Dogs</li><li>Cats</li><li>Rabbits</li></ul>")
 })
 
+app.get('/animals', (req,res)=>{
+    res.send("<h1> List of pets</h1>")
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server started on ${PORT}.`);
 })
