e125055dff06ff6f4e86c2f63d27d3acfc3ade13
valeviit <valeviit@icloud.com>
2022-11-18 13:33:41 +0100

    add an <a> element in each <li> element from /

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Adopt a Pet!</h1><p>Browse through the links below to find your new furry friend:</p><ul><li>Dogs</li><li>Cats</li><li>Rabbits</li></ul>")
+    res.send(`<h1>Adopt a Pet!</h1><p>Browse through the links below to find your new furry friend:</p><ul><li><a href="/animals/dogs">Dogs</a></li><li><a href="/animals/cats">Cats</a></li><li><a href="/animals/rabbits">Rabbits</a></li></ul>`)
 })
 
 app.get('/animals', (req,res)=>{
