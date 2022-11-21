9fb773499a21db4856a4f5d06527fdf52d891018
valeviit <valeviit@icloud.com>
2022-11-21 23:04:06 +0100

    point 15 , 16 and 17

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const pets = require("./helper.js");
-const obj = JSON.stringify(pets);
+//const obj = JSON.stringify(pets);
 
 //app.use(express.json());
 app.get("/", (req, res)=>{
@@ -15,15 +15,17 @@ app.get('/animals', (req,res)=>{
 
 app.get("/animals/:pet_type", (req,res,next)=>{   
     const { pet_type } =req.params;
-    console.log({pet_type})
+    //console.log({pet_type})
     const html = `<h1>List of ${pet_type}</h1> <ul>
-     ${pets[pet_type].map((pet,index)=>` <li>${pet.name}</li>`).join('')}</ul>`;
+     ${pets[pet_type].map((pet,index)=>` <li><a href= /animals/${pet_type}/${index}>${pet.name}</a></li>`).join('')}</ul>`;
     res.send(html);
 })
 
-app.get("/animals/:pet_type/:pet_id", (req,res,next)=>{
+app.get("/animals/:pet_type/:pet_id", (req,res)=>{
     const {pet_type, pet_id} = req.params;
-    const animal =  pets[pet_type][pet_id];
+   //console.log(pets[pet_type][pet_id])
+   const pet= pets[pet_type][pet_id];
+    res.send(`<h1>${pet.name}</h1><img src=${pet.url}/> <p>${pet.description}</p> <ul><li>${pet.breed}</li><li>${pet.age}</li></ul>`)
 })
  
 
