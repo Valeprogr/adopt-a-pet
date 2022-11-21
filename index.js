458312248df730e8345cf89ffa3de62641bf1840
valeviit <valeviit@icloud.com>
2022-11-21 01:47:03 +0100

    point 12

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const pets = require("./helper.js");
-console.log(`${JSON.stringify(pets)}`)
+const obj = JSON.stringify(pets);
 
-app.use(express.json());
+//app.use(express.json());
 app.get("/", (req, res)=>{
     res.send(`<h1>Adopt a Pet!</h1><p>Browse through the links below to find your new furry friend:</p><ul><li><a href="/animals/dogs">Dogs</a></li><li><a href="/animals/cats">Cats</a></li><li><a href="/animals/rabbits">Rabbits</a></li></ul>`)
 })
 
 app.get('/animals', (req,res)=>{
-    res.send("<h1> List of pets</h1>")
+    res.send("<h1>List of pets</h1>");
 })
 
 app.get("/animals/:pet_type", (req,res,next)=>{   
-    console.log(req.params.pet_type);
-    res.send(`<h1>List of ${req.params.pet_type}</h1>`);
+    const { pet_type } =req.params;
+    console.log({pet_type})
+    const html = `<h1>List of ${pet_type}</h1> <ul>
+     ${pets[pet_type].map((pet,index)=>` <li>${pet.name}</li>`).join('')}</ul>`;
+    res.send(html);
 })
  
 
